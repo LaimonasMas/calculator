a68eacd3f1d5684221aa4cb3bec73087a029b32c
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-22 00:56:42 +0200

    added main.css and img

--- resources/views/calculator/result.blade.php
+++ resources/views/calculator/result.blade.php
@@ -1,8 +1,10 @@
 @extends('layout.main')
 
 @section('content')
+<img src="http://localhost/nd/nd_10calc/calculator/public/img/download.png">
 <h1>Atsakymas</h1>
 <h1> {{ $result }} </h1>
+
 
 @endsection
 
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link href="http://localhost/nd/nd_10calc/calculator/public/css/main.css?ver=<?= time() ?>" rel="stylesheet">
 </head>
 <body>
     @yield('content')
