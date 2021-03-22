bdd8dab783249e13a56eb555f756f4c6fbc78c1d
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-22 10:28:57 +0200

    back to one result.blade.php

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -6,23 +6,28 @@ use Illuminate\Http\Request;
 
 class CalculatorController extends Controller
 {
+
     public function add($number1, $number2)
     {
-        return view('calculator.add',['number1' => $number1, 'number2' => $number2]);
+        $result = $number1 + $number2;
+        return view('calculator.result',['result' => $result]); 
     }
 
     public function subtract($number1, $number2)
     {
-        return view('calculator.subtract',['number1' => $number1, 'number2' => $number2]);
+        $result = $number1 - $number2;
+        return view('calculator.result',['result' => $result]); 
     }
 
     public function multiply($number1, $number2)
     {
-        return view('calculator.multiply',['number1' => $number1, 'number2' => $number2]); 
+        $result = $number1 * $number2;
+        return view('calculator.result',['result' => $result]); 
     }
 
     public function divide($number1, $number2)
     {
-        return view('calculator.divide',['number1' => $number1, 'number2' => $number2]); 
+        $result = $number1 / $number2;
+        return view('calculator.result',['result' => $result]); 
     }
 }
--- resources/views/calculator/divide.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main')
-@section('content')
-<img src="http://localhost/nd/nd_10calc/calculator/public/img/download.png">
-<h1>Atsakymas</h1>
-<h1> {{ $number1 / $number2 }} </h1>
-@endsection
-@section('title') Calculator @endsection
